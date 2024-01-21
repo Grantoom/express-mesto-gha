257340f1fe7b589f8ecd4f9cfb339c97b8f46bde
Maxim Kovalev <wolverin12@ya.ru>
2024-01-21 17:00:23 +0500

    fix

--- routes/cards.js
+++ routes/cards.js
@@ -9,13 +9,9 @@ const {
 } = require('../controllers/cards');
 
 router.get('/', getCards);
-
 router.post('/', createCard);
-
 router.delete('/:cardId', deleteCard);
-
 router.put('/:cardId/likes', likedCard);
-
 router.delete('/:cardId/likes', dislikedCard);
 
 module.exports = router;
